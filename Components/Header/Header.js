186ec376ec91fe120399df891486ffda1f57be75
Allyn McKenna Patterson <allyncodes@gmail.com>
2023-12-13 01:01:26 +0000

    added logo

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -7,7 +7,6 @@ import { FaUser } from "react-icons/fa";
 import GlobalContext from "@/Store/globalContext";
 import Link from "next/link";
 import { IconContext } from "react-icons";
-// import myLogo from "./logo.png";
 
 function Header() {
   const globalCtx = useContext(GlobalContext);
@@ -20,9 +19,15 @@ function Header() {
             <p className={classes.buttonText}>Home</p>
           </Link>
         </div>
-        {/* <div className={classes.logoContainer}>
-          <img src={myLogo} alt="picSync logo" />
-        </div> */}
+        <div>
+          <div className={classes.logoContainer}>
+            <img
+              src="assets/images/logo.png"
+              alt="picSync logo"
+              className={classes.logo}
+            />
+          </div>
+        </div>
         <div className={classes.iconsContainer}>
           <IconContext.Provider
             value={{ color: "#000", size: "30px", marginRight: "5px" }}
@@ -43,11 +48,15 @@ function Header() {
             <p className={classes.buttonText}>Home</p>
           </Link>
         </div>
-        {/* <div>
+        <div>
           <div className={classes.logoContainer}>
-            <a src={myLogo} alt="picSync logo" className={classes.logo} />
+            <img
+              src="assets/images/logo.png"
+              alt="picSync logo"
+              className={classes.logo}
+            />
           </div>
-        </div> */}
+        </div>
         <div className={classes.logIn}>
           <Link className={classes.link} href="/LogIn">
             <p className={classes.buttonText}>Log In</p>
