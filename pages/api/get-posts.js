bb3b841ae341c401d60d77848a0484a37d687e87
G00398070 <G00398070@atu.ie>
2023-12-29 11:49:34 +0000

    changed the routes in the api to the cicd-main container
    Signed-off-by: G00398070 <G00398070@atu.ie>

--- pages/api/get-posts.js
+++ pages/api/get-posts.js
@@ -1,6 +1,7 @@
 async function handler(req, res) {
   console.log("Posting data to database");
-  const response = await fetch("http://localhost:8081/main", {
+  // const response = await fetch("http://localhost:8081/main", {
+  const response = await fetch("http://cicd-main:8081/main", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
