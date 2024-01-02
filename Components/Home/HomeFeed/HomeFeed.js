bba7150a09269962997ead7f58746b3ee25f1472
Allyn McKenna Patterson <allyncodes@gmail.com>
2024-01-02 23:34:19 +0000

    Added FollowList to homefeed

--- Components/Home/HomeFeed/HomeFeed.js
+++ Components/Home/HomeFeed/HomeFeed.js
@@ -1,21 +1,36 @@
 import classes from "./HomeFeed.module.css";
+import { useContext, useState } from "react";
 import Post from "../Post/Post";
 import Card from "@/Components/Card/Card";
+import UserList from "@/Components/UserList/UserList";
+import GlobalContext from "@/Store/globalContext";
 
 function HomeFeed(props) {
+
+  const globalCtx = useContext(GlobalContext);
   return (
     <div className={classes.mainPostContainer}>
+      <div className={classes.userListContainer}>
+        <UserList users={globalCtx.theGlobalObject.users}/>
+      </div>
+        
       <ul className={classes.list}>
-        {props.posts.map((post) => (
-          <Card>
+        {props.posts.map((post, currentUser) => (
+          <div className={classes.postContainer}>
+            <Card>
             <Post
-            username={post.userName}
-            image={post.imgString}
-            text={post.postText}
-          />
+              username={post.userName}
+              image={post.imgString}
+              text={post.postText}
+              profilepic={currentUser.profilepic}
+            />
           </Card>
+          </div>
+          
         ))}
       </ul>
+      <div className={classes.userListContainer}>
+      </div>
     </div>
   );
 }
