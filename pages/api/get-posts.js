9e9a70ee1388a552be2113c57409cdb3d328c5f9
G00398070 <G00398070@atu.ie>
2024-01-03 00:16:07 +0000

    commented redundant line
    Signed-off-by: G00398070 <G00398070@atu.ie>

--- pages/api/get-posts.js
+++ pages/api/get-posts.js
@@ -1,5 +1,5 @@
 async function handler(req, res) {
-  console.log("Posting data to database");
+  // console.log("Posting data to database");
   const response = await fetch("http://localhost:8081/main", {
   // const response = await fetch("http://cicd-main:8081/main", {
     method: "GET",
