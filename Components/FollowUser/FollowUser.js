9af658f5b5ae6d447d357097d094135864815ce7
Allyn McKenna Patterson <allyncodes@gmail.com>
2024-01-06 10:49:57 +0000

    User name and profile picture are now taken from global context

--- Components/FollowUser/FollowUser.js
+++ Components/FollowUser/FollowUser.js
@@ -6,18 +6,13 @@ import Link from "next/link";
 export default function FollowUser(props) {
   return (
     <div className={classes.userContainer}>
-      {/* <img src={props.profilePic} />
-            <p>{props.username}</p> */}
       <div className={classes.profilePic}>
-        <img src="https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg" />
+        <img src={props.profilepic} />
       </div>
-      <p className={classes.username}>User</p>
-      {/* <div className={classes.link}>
-        Follow
-      </div> */}
+      <p className={classes.username}>{props.username}</p>
       <Link className={classes.link} href="/">
-            <p className={classes.buttonText}>Follow</p>
-        </Link>
+        <p className={classes.buttonText}>Follow</p>
+      </Link>
     </div>
   );
 }
