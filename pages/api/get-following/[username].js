6eca10d199caa31f5740bc852bf9fc614951c500
G00398070 <G00398070@atu.ie>
2024-01-09 12:21:09 +0000

    again
    Signed-off-by: G00398070 <G00398070@atu.ie>

--- pages/api/get-following/[username].js
+++ pages/api/get-following/[username].js
@@ -1,10 +1,10 @@
 async function handler(req, res) {
-  const {username} = req.query
+  const { username } = req.query
   console.log("Getting user's following list");
   // const response = await fetch(`http://localhost:8081/getFollowList/${username}`, {
   const response = await fetch(`http://cicd-main:8081/getFollowList/${username}`, {
     method: "GET",
-    
+
   });
   const data = await response.json()
   console.log(data)
--- pages/api/init-following/[username].js
+++ pages/api/init-following/[username].js
@@ -1,5 +1,5 @@
 async function handler(req, res) {
-  const {username} = req.query
+  const { username } = req.query
   console.log("Initializing user's following list");
   // const response = await fetch(`http://localhost:8081/initFollow/${username}`, {
   const response = await fetch(`http://cicd-main:8081/initFollow/${username}`, {
