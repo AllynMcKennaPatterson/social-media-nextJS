a1f9407b23953fd708f4a7ade0947faae2ce947e
Allyn McKenna Patterson <allyncodes@gmail.com>
2024-01-09 11:30:24 +0000

    Added conditional rendering

--- Components/UserList/UserList.js
+++ Components/UserList/UserList.js
@@ -6,7 +6,8 @@ import FollowUser from "../FollowUser/FollowUser";
 
 export default function UserList(props) {
     
-    return (
+    if(props.method == "follow"){
+        return (
         <div className={classes.userListContainer}>
            <Card>
                 <h3>Follow Other Users</h3>
@@ -15,6 +16,7 @@ export default function UserList(props) {
                     <li key={user.username}  className={classes.user}>
                         <Card>
                         <FollowUser
+                        method = {props.method}
                         username={user.username}
                         profilepic={user.profilepic}
                         />
@@ -27,5 +29,34 @@ export default function UserList(props) {
         </div>
                
     );
+    }
+    else if(props.method == "unfollow"){
+        if(props.users == 0){
+            return;
+        }
+        return (
+            <div className={classes.userListContainer}>
+               <Card>
+                    <h3>Unfollow Users</h3>
+                    <ul className={classes.list}>
+                        {props.users.map((user) => (
+                        <li key={user.username}  className={classes.user}>
+                            <Card>
+                            <FollowUser
+                            method = {props.method}
+                            username={user.username}
+                            profilepic={user.profilepic}
+                            />
+                        </Card>
+                        </li>
+                        
+                        ))}
+                    </ul>
+                </Card>   
+            </div>
+                   
+        );
+    }
+    
   }
   
