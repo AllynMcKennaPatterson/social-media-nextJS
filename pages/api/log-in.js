912b65b147c53d84973890c93282623541455d9b
G00398070 <G00398070@atu.ie>
2024-01-09 12:20:43 +0000

    change api routes to connect to images instead of localhost
    Signed-off-by: G00398070 <G00398070@atu.ie>

--- pages/api/log-in.js
+++ pages/api/log-in.js
@@ -1,7 +1,7 @@
 async function handler(req, res) {
   // console.log("Posting data to database");
-  const response = await fetch("http://localhost:8081/login", {
-    // const response = await fetch("http://cicd-main:8081/login", {
+  // const response = await fetch("http://localhost:8081/login", {
+  const response = await fetch("http://cicd-main:8081/login", {
     method: "POST",
     body: JSON.stringify(req.body),
     headers: {
--- pages/api/sign-up.js
+++ pages/api/sign-up.js
@@ -1,6 +1,6 @@
 async function handler(req, res) {
-  const response = await fetch("http://localhost:8081/signup", {
-  // const response = await fetch("http://cicd-main:8081/signup", {
+  // const response = await fetch("http://localhost:8081/signup", {
+  const response = await fetch("http://cicd-main:8081/signup", {
     method: "POST",
     body: JSON.stringify(req.body),
     headers: {
