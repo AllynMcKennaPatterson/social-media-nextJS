d994e82f0d89710b86b5406dcc8b15a02b608849
Allyn McKenna Patterson <allyncodes@gmail.com>
2024-01-10 19:12:04 +0000

    Added container url

--- pages/api/log-in-secure/[username].js
+++ pages/api/log-in-secure/[username].js
@@ -2,7 +2,8 @@ import CompareHashandPlain from "@/functions/Bcrypt/CompareHashandPlain";
 async function handler(req, res) {
   const { username } = req.query
 
-  const response = await fetch(`http://localhost:8081/getPassword/${username}`, {
+  // const response = await fetch(`http://localhost:8081/getPassword/${username}`, {
+  const response = await fetch(`http://cicd-main:8081/getPassword/${username}`, {
     method: "GET"
   });
   const data = await response.json();
